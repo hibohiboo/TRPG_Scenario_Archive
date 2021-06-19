aa8ce57a81a74f09c016121e1eb30a9580a8b635
hibohiboo <swdice66@gmail.com>
2021-06-19 12:42:46 +0900

    アコーディオン 追加

--- front/front/src/components/organisms/Udoncard.vue
+++ front/front/src/components/organisms/Udoncard.vue
@@ -4,7 +4,41 @@
       <div id="konva-container" />
     </div>
     <div class="p-col-4">
-      <Textarea v-for="item in store.data.children[0].children[1].children" :key="item" v-model="item.attrs.text" :auto-resize="true" />
+      <Accordion :active-index="0">
+        <AccordionTab header="テキスト">
+          <Card v-for="item in store.state.data.children[layerIndex].children[textGroupIndex].children" :key="item">
+            <template #content>
+              <Textarea v-model="item.attrs.text" :auto-resize="true" />
+              <label style="display:block">
+                x:
+                <InputText
+                  id="inputtext"
+                  v-model.number="item.attrs.x"
+                  type="number"
+                />
+              </label>
+              <label style="display:block">
+                y:
+                <InputText
+                  id="inputtext"
+                  v-model.number="item.attrs.y"
+                  type="number"
+                />
+              </label>
+            </template>
+          </Card>
+          <div class="p-grid" style="margin: 10px">
+            <Button label="追加" icon="pi pi-plus" icon-pos="right" style="display:block" class="p-offset-1 p-col" />
+            <Button label="削除" icon="pi pi-minus" icon-pos="right" style="display:block" class="p-offset-2 p-col p-button-danger" />
+          </div>
+        </AccordionTab>
+        <AccordionTab header="Header II">
+          Content
+        </AccordionTab>
+        <AccordionTab header="Header III">
+          Content
+        </AccordionTab>
+      </Accordion>
     </div>
   </div>
 </template>
@@ -15,13 +49,24 @@ import {
   ref, defineComponent, onMounted, watchEffect, watch,
 } from 'vue';
 import Textarea from 'primevue/textarea';
+import Accordion from 'primevue/accordion';
+import AccordionTab from 'primevue/accordiontab';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+
 // eslint-disable-next-line import/extensions
 import { setup } from '../../domain/udoncard/setup';
 
 export default defineComponent({
   name: 'Udoncard',
-  components: { Textarea },
+  components: {
+    Textarea, Accordion, AccordionTab, Button, Card,
+  },
   setup: () => setup(),
+  computed: {
+    layerIndex() { return 0; },
+    textGroupIndex() { return 1; },
+  },
 });
 </script>
 
