28de2c43f3585027f87ce6496cab78f1ba5a3d3d
hibohiboo <swdice66@gmail.com>
2021-06-19 09:28:58 +0900

    fix  このデータ構造は没っぽい。xを動かしたときとtextを変えたときで遅延がすごい

--- front/front/src/domain/udoncard/draw.ts
+++ front/front/src/domain/udoncard/draw.ts
@@ -4,14 +4,17 @@ import type { ContainerConfig } from 'konva/lib/Container'
 import type { Stage } from 'konva/lib/Stage';
 
 
-export const draw = (data: ContainerConfig) => {
+export const draw = (data: ContainerConfig, updateData: (d: ContainerConfig) => void) => {
   // first we need to create a stage
+  console.log('elm', document.getElementById('konva-container'))
   var stage: Stage = Konva.Node.create(data, 'konva-container');
 
   // then create layer
   var layer = stage.find('Layer')
+  stage.off('dragend');
   stage.on('dragend', async (event: any) => {
-    data.children = event.currentTarget.children
+    console.log('drageend')
+    updateData(event.currentTarget)
   })
   layer[0].draw();
   // saveData(stage.toJSON())
--- front/front/src/domain/udoncard/setup.ts
+++ front/front/src/domain/udoncard/setup.ts
@@ -1,20 +1,24 @@
+import { ContainerConfig } from 'konva/lib/Container';
 import {
   ref, defineComponent, onMounted, watchEffect, watch, reactive
 } from 'vue';
 import { loadData } from '../store';
 import { draw } from './draw'
-
+import cloneDeep from 'lodash/cloneDeep'
 export const setup = () => {
   const data = reactive(loadData());
+  const updateData = (d: ContainerConfig) => {
+    console.log('test', d)
+    data.children = d.children;
+  }
 
   // watchEffectだと #konva-container が描画される前に動いてしまう
-  watch([data], () => {
-    draw(data);
-  },
-    { deep: true });
+  // watch(() => cloneDeep(data), (d) => {
+  //   draw(d, updateData);
+  // }, { deep: true });
 
   onMounted(() => {
-    draw(data);
+    draw(data, updateData);
   });
   return {
     data
