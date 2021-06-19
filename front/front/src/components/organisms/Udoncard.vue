741e30b8f645f9b110fc7bf5420fd26154f3f848
hibohiboo <swdice66@gmail.com>
2021-06-19 14:28:21 +0900

    文字詳細設定を追加

--- front/front/src/components/organisms/Udoncard.vue
+++ front/front/src/components/organisms/Udoncard.vue
@@ -6,9 +6,10 @@
     <div class="p-col">
       <Accordion :active-index="0">
         <AccordionTab header="テキスト">
-          <Card v-for="item in store.state.data.children[store.const.layerIndex].children[store.const.textGroupIndex].children" :key="item">
-            <template #content>
-              <Textarea v-model="item.attrs.text" :auto-resize="true" />
+          <div v-for="item in store.state.data.children[store.const.layerIndex].children[store.const.textGroupIndex].children" :key="item" class="sa-edit-item">
+            <Textarea v-model="item.attrs.text" :auto-resize="true" />
+            <details>
+              <summary>位置</summary>
               <label style="display:block">
                 x:
                 <InputText
@@ -23,84 +24,113 @@
                   type="number"
                 />
               </label>
-            </template>
-          </Card>
+            </details>
+            <details>
+              <summary>詳細</summary>
+              <label style="display:block">
+                サイズ:
+                <InputText
+                  v-model.number="item.attrs.fontSize"
+                  type="number"
+                />
+                <label style="display:block">
+                  文字色:
+                  <InputText
+                    v-model.number="item.attrs.fill"
+                    type="color"
+                  />
+                </label>
+                <label style="display:block">
+                  文字位置(left/center/right):
+                  <InputText
+                    v-model.number="item.attrs.align"
+                    type="text"
+                  />
+                </label>
+                <label style="display:block">
+                  幅:
+                  <InputText
+                    v-model.number="item.attrs.width"
+                    type="number"
+                  />
+                </label>
+              </label>
+            </details>
+          </div>
           <div class="p-grid" style="margin: 10px">
             <Button label="追加" icon="pi pi-plus" icon-pos="right" style="display:block" class="p-offset-1 p-col" @click="()=>store.addNewText()" />
             <Button label="削除" icon="pi pi-minus" icon-pos="right" style="display:block" class="p-offset-2 p-col p-button-danger" />
           </div>
         </AccordionTab>
         <AccordionTab header="枠">
-          <Card v-for="item in store.state.data.children[store.const.layerIndex].children[store.const.frameGroupIndex].children" :key="item">
-            <template #content>
+          <div v-for="item in store.state.data.children[store.const.layerIndex].children[store.const.frameGroupIndex].children" :key="item" class="sa-edit-item">
+            <label style="display:block">
+              width:
+              <InputText
+                v-model.number="item.attrs.width"
+                type="number"
+                min="0"
+              />
+            </label>
+            <label style="display:block">
+              height:
+              <InputText
+                v-model.number="item.attrs.height"
+                type="number"
+                min="0"
+              />
+            </label>
+            <details>
+              <summary>位置</summary>
               <label style="display:block">
-                width:
+                x:
                 <InputText
-                  v-model.number="item.attrs.width"
+                  v-model.number="item.attrs.x"
+                  type="number"
+                />
+              </label>
+              <label style="display:block">
+                y:
+                <InputText
+                  v-model.number="item.attrs.y"
+                  type="number"
+                />
+              </label>
+            </details>
+            <details>
+              <summary>詳細設定</summary>
+              <label style="display:block">
+                枠線幅:
+                <InputText
+                  v-model.number="item.attrs.strokeWidth"
                   type="number"
                   min="0"
                 />
               </label>
               <label style="display:block">
-                height:
+                線の色:
+                <input
+                  v-model.number="item.attrs.stroke"
+                  type="color"
+                >
+              </label>
+              <label style="display:block">
+                塗りつぶしの色:
+                <input
+                  v-model.number="item.attrs.fill"
+                  type="color"
+                >
+              </label>
+              <label style="display:block">
+                角丸:
                 <InputText
-                  v-model.number="item.attrs.height"
+                  v-model.number="item.attrs.cornerRadius"
                   type="number"
                   min="0"
                 />
               </label>
-              <details>
-                <summary>位置詳細</summary>
-                <label style="display:block">
-                  x:
-                  <InputText
-                    v-model.number="item.attrs.x"
-                    type="number"
-                  />
-                </label>
-                <label style="display:block">
-                  y:
-                  <InputText
-                    v-model.number="item.attrs.y"
-                    type="number"
-                  />
-                </label>
-              </details>
-              <details>
-                <summary>詳細設定</summary>
-                <label style="display:block">
-                  枠線幅:
-                  <InputText
-                    v-model.number="item.attrs.strokeWidth"
-                    type="number"
-                    min="0"
-                  />
-                </label>
-                <label style="display:block">
-                  線の色:
-                  <input
-                    v-model.number="item.attrs.stroke"
-                    type="color"
-                  >
-                </label>
-                <label style="display:block">
-                  塗りつぶしの色:
-                  <input
-                    v-model.number="item.attrs.fill"
-                    type="color"
-                  >
-                </label>
-                <label style="display:block">
-                  角丸:
-                  <InputText
-                    v-model.number="item.attrs.cornerRadius"
-                    type="number"
-                    min="0"
-                  />
-                </label>
-              </details>
-            </template>
-          </Card>
+            </details>
+          </div>
           <div class="p-grid" style="margin: 10px">
             <Button label="追加" icon="pi pi-plus" icon-pos="right" style="display:block" class="p-offset-1 p-col" @click="()=>store.addNewText()" />
             <Button label="削除" icon="pi pi-minus" icon-pos="right" style="display:block" class="p-offset-2 p-col p-button-danger" />
@@ -131,7 +161,7 @@ import { setup } from '../../domain/udoncard/setup';
 export default defineComponent({
   name: 'Udoncard',
   components: {
-    Textarea, Accordion, AccordionTab, Button, Card,
+    Textarea, Accordion, AccordionTab, Button,
   },
   setup: () => setup(),
 });
@@ -146,5 +176,9 @@ export default defineComponent({
   .sa-co-width{
     width:100%
   }
-
+.sa-edit-item{
+  text-align: left;
+  padding: 10px;
+  border-bottom: 1px solid #222;
+}
 </style>
