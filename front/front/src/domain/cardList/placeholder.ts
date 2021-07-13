9cc6ee37aa6edbec8eef22be2398ac23924fb484
hibohiboo <swdice66@gmail.com>
2021-07-14 08:31:06 +0900

    fix "の考慮

--- front/front/src/domain/cardList/placeholder.ts
+++ front/front/src/domain/cardList/placeholder.ts
@@ -1,7 +1,8 @@
 export const replacePlaceHolder = (csv: string, json: string) => {
   let result = json;
   csv.split(',').forEach((t, i) => {
-    result = result.replaceAll(new RegExp(`\\$${i + 1}`, 'g'), t);
+    const text = t.replaceAll('"', '\\"');
+    result = result.replaceAll(new RegExp(`\\$${i + 1}`, 'g'), text);
   });
   return result;
 };
