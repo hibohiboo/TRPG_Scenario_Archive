45fa14da9a25eb9c1249ffeeae7da5f60d6d74a1
hibohiboo <swdice66@gmail.com>
2021-09-05 11:48:46 +0900

    2桁の変換が崩れる問題を修正

--- front/front/src/domain/cardList/placeholder.ts
+++ front/front/src/domain/cardList/placeholder.ts
@@ -1,11 +1,13 @@
 export const replacePlaceHolder = (csv: string[], json: string) => {
   let result = json;
-  csv.forEach((t, i) => {
+  for (let i = csv.length - 1; i > -1; i--) {
+    const t = csv[i];
     // 置換するテキストを無害化
     const text = JSON.stringify(t).slice(1).slice(0, -1).replaceAll("\\\\n", "\\n");
 
     result = result.replaceAll(new RegExp(`\\$${i + 1}`, 'g'), text);
-  });
+  }
+
 
   return result;
 };
